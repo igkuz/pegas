2d5d618be5d307aa44e348ba0c02f4bb793f1b6b
Igor Kuznetsov <igkuznetsov@gmail.com>
2013-06-01 12:24:15 +0400

    Modified binary, added start to runner

--- lib/pegas/runner.rb
+++ lib/pegas/runner.rb
@@ -1,10 +1,14 @@
+require 'rack'
+
 module Pegas
 
   class Runner
 
-
-    def self.start
-
+    def self.start(app_path)
+      raw = File.read(app_path)
+      raw.sub!(/^__END__\n.*/, '')
+      inner_app = eval("Rack::Builder.new {(\n#{raw}\n)}.to_app", TOPLEVEL_BINDING, app_path)
+      p inner_app
     end
   end
 end
